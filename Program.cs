e5a1dd3181ee5916cff4d88f208701feab1cec43
GAleksiev <122028615+GAleksiev@users.noreply.github.com>
2023-01-19 23:35:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,160 @@
+﻿namespace ConsoleApp2 {
+    internal class Program {
+        static void Main(string[] args) {
+            //__________Uprajnenie 1__________//
+
+            //            using System;
+            //namespace HelloSoftuni {
+            //        internal class Program {
+            //            static void Main(string[] args) {
+            //               
+            //              Write Code Here
+            //
+            //            }
+            //        }
+            //    }
+
+            ////Zadacha 01. Hello SoftUni                                                                                                                              Result: 100/100
+            //Console.WriteLine("Hello SoftUni");
+
+            ////Zadacha 02. Nums 1...10                                                                                                                                Result: 100/100
+            //Console.WriteLine("1");
+            //Console.WriteLine("2");
+            //Console.WriteLine("3");
+            //Console.WriteLine("4");
+            //Console.WriteLine("5");
+            //Console.WriteLine("6");
+            //Console.WriteLine("7");
+            //Console.WriteLine("8");
+            //Console.WriteLine("9");
+            //Console.WriteLine("10");
+
+            ////Zadacha 03. Rectangle Area                                                                                                                             Result: 100/100
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int area = a * b;
+            //Console.WriteLine(area);
+
+            ////Zadacha 04. Inches to Centimeters                                                                                                                      Result: 100/100
+            //double cm = double.Parse(Console.ReadLine())*2.54;
+            //Console.WriteLine(cm);
+
+            ////Zadacha 05. Greeting by Name                                                                                                                           Result: 100/100
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Hello, {name}!");
+
+            ////Zadacha 06. Concatenate Data                                                                                                                           Result: 100/100
+            //string firstName = Console.ReadLine();
+            //string lastName = Console.ReadLine();
+            //int age = int.Parse(Console.ReadLine());
+            //string town = Console.ReadLine();
+            //Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+
+            ////Zadacha 07. Projects Creation                                                                                                                          Result: 100/100
+            //string name = Console.ReadLine();
+            //int projectNum = int.Parse(Console.ReadLine());
+            //int hours = projectNum * 3;
+            //Console.WriteLine($"The architect {name} will need {hours} hours to complete {projectNum} project/s.");
+
+            ////Zadacha 08. Pet Shop                                                                                                                                   Result: 100/100
+            //int dogFood = int.Parse(Console.ReadLine());
+            //int catFood = int.Parse(Console.ReadLine())*4;
+            //double fullPrice = (dogFood * 2.50)+catFood;
+            //Console.WriteLine($"{fullPrice} lv.");
+
+            //////Zadacha 09. Yard Greening                                                                                                                            Result: 100/100
+            //double kvMetri = double.Parse(Console.ReadLine())*7.61;
+            //double discount = kvMetri * 0.18;
+            //double finalPrice = kvMetri - discount;
+            //Console.WriteLine($"The final price is: {finalPrice} lv.\nThe discount is: {discount} lv.");
+
+            //__________Uprajnenie 2__________//
+
+            //            using System;
+            //namespace HelloSoftuni {
+            //        internal class Program {
+            //            static void Main(string[] args) {
+            //               
+            //              Write Code Here
+            //
+            //            }
+            //        }
+            //    }
+
+
+            //Zadacha 01. USD to BGN                                                                                                                                   Result: 100/100
+            //double BGN =double.Parse(Console.ReadLine())*1.79549;
+            //Console.WriteLine(BGN);
+
+            //Zadacha 02. Radians to Degrees                                                                                                                           Result: 100/100
+            //double degrees = double.Parse(Console.ReadLine())*180/Math.PI;
+            //Console.WriteLine(degrees);
+
+            ////Zadacha 03. Deposit Calculator                                                                                                                         Result: 100/100
+            //double deposit = double.Parse(Console.ReadLine());
+            //int months = int.Parse(Console.ReadLine());
+            //double yearlyInterest = double.Parse(Console.ReadLine())/100;
+            //double monthlyInterest = (deposit * yearlyInterest) / 12;
+            //Console.WriteLine(deposit + months * monthlyInterest );
+
+            ////Zadacha 04. Vacation Books List                                                                                                                        Result: 100/100
+            //int bookPages = int.Parse(Console.ReadLine());
+            //int pagePerHour = int.Parse(Console.ReadLine()); 
+            //int days = int.Parse(Console.ReadLine());          
+            //Console.WriteLine((bookPages/pagePerHour)/days);
+
+            ////Zadacha 05. Supplies for School                                                                                                                        Result: 100/100
+            //int pens = int.Parse(Console.ReadLine());
+            //int markers = int.Parse(Console.ReadLine());
+            //int chemical = int.Parse(Console.ReadLine());
+            //double discountNum = double.Parse(Console.ReadLine())/100;
+            //double fullPrice = pens * 5.80 + markers * 7.20 + chemical * 1.20;
+            //double discountPrice =(fullPrice)*discountNum;
+            //Console.WriteLine(fullPrice-discountPrice);
+
+            ////Zadacha 06. Repainting                                                                                                                                 Result: 100/100
+            //double bags = 0.40;
+            //int nylon = int.Parse(Console.ReadLine());
+            //int paint = int.Parse(Console.ReadLine()); 
+            //int thinner = int.Parse(Console.ReadLine());  
+            //int time = int.Parse(Console.ReadLine());
+            //double nylonPrice = (2+nylon) * 1.50;
+            //double paintPrice = ((paint*0.1)+paint) * 14.50;
+            //double thinnerPrice = thinner * 5.00;
+            //double materialsPrice = nylonPrice + paintPrice + thinnerPrice + bags;
+            //double hourlyPay =time*((nylonPrice+paintPrice+thinnerPrice+bags)*0.3);
+            //Console.WriteLine(hourlyPay+materialsPrice);
+
+            ////Zadacha 07. Food Delivery                                                                                                                              Result: 100/100
+            //int chickenMenu = int.Parse(Console.ReadLine());
+            //int fishMenu = int.Parse(Console.ReadLine());
+            //int vegetarianMenu = int.Parse(Console.ReadLine());
+            //double chickenMenuPrice = chickenMenu * 10.35;
+            //double fishMenuPrice = fishMenu * 12.40;
+            //double vegetarianMenuPrice = vegetarianMenu * 8.15;
+            //double menusPrice=chickenMenuPrice+fishMenuPrice+vegetarianMenuPrice;
+            //Console.WriteLine((menusPrice * 0.2) + menusPrice+2.50);
+
+
+
+            ////Zadacha 08. Basketball Equipment                                                                                                                       Result: 100/100
+            //int subscription = int.Parse(Console.ReadLine());
+            //double shoesBasketball = subscription - (subscription * 0.4);
+            //double clothesBasketball = shoesBasketball-(shoesBasketball * 0.2);
+            //double ballBasketball = clothesBasketball / 4;
+            //double accessoriesBasketball = ballBasketball / 5;
+            //Console.WriteLine(shoesBasketball+clothesBasketball+ballBasketball+accessoriesBasketball+subscription);
+
+            ////Zadacha 09. Fish Tank                                                                                                                                  Result: 100/100
+            //int lengthCm = int.Parse(Console.ReadLine());
+            //int widthCm = int.Parse(Console.ReadLine());  
+            //int heigthCm = int.Parse(Console.ReadLine());
+            //double digit = double.Parse(Console.ReadLine());
+            //double percent = 1-(digit/100);
+            //double obemL = (lengthCm * widthCm * heigthCm) * 0.001;
+            //Console.WriteLine(obemL*percent);
+
+
+        }
+    }
+}
