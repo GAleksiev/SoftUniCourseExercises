3354471ec3e1301400e5aca8e7b28362b185f212
GAleksiev <122028615+GAleksiev@users.noreply.github.com>
2023-03-06 13:00:04 +0200

    Add files via upload

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Exercise_1 {
+    internal class Program {
+        static void Main(string[] args) {
+
+    //        using System;
+    //                namespace HelloSoftuni
+    //{
+    //    internal class Program
+    //    {
+    //        static void Main(string[] args)
+    //        {
+
+    //            Write Code Here
+
+
+    //                            }
+    //    }
+    //}
+
+    //__________Uprajnenie 1__________//
+
+    ////Zadacha 01. Hello SoftUni                                                                                                                              Result: 100/100
+    //Console.WriteLine("Hello SoftUni");
+
+    ////Zadacha 02. Nums 1...10                                                                                                                                Result: 100/100
+    //Console.WriteLine("1");
+    //Console.WriteLine("2");
+    //Console.WriteLine("3");
+    //Console.WriteLine("4");
+    //Console.WriteLine("5");
+    //Console.WriteLine("6");
+    //Console.WriteLine("7");
+    //Console.WriteLine("8");
+    //Console.WriteLine("9");
+    //Console.WriteLine("10");
+
+    ////Zadacha 03. Rectangle Area                                                                                                                             Result: 100/100
+    //int a = int.Parse(Console.ReadLine());
+    //int b = int.Parse(Console.ReadLine());
+    //int area = a * b;
+    //Console.WriteLine(area);
+
+    ////Zadacha 04. Inches to Centimeters                                                                                                                      Result: 100/100
+    //double cm = double.Parse(Console.ReadLine())*2.54;
+    //Console.WriteLine(cm);
+
+    ////Zadacha 05. Greeting by Name                                                                                                                           Result: 100/100
+    //string name = Console.ReadLine();
+    //Console.WriteLine($"Hello, {name}!");
+
+    ////Zadacha 06. Concatenate Data                                                                                                                           Result: 100/100
+    //string firstName = Console.ReadLine();
+    //string lastName = Console.ReadLine();
+    //int age = int.Parse(Console.ReadLine());
+    //string town = Console.ReadLine();
+    //Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+
+    ////Zadacha 07. Projects Creation                                                                                                                          Result: 100/100
+    //string name = Console.ReadLine();
+    //int projectNum = int.Parse(Console.ReadLine());
+    //int hours = projectNum * 3;
+    //Console.WriteLine($"The architect {name} will need {hours} hours to complete {projectNum} project/s.");
+
+    ////Zadacha 08. Pet Shop                                                                                                                                   Result: 100/100
+    //int dogFood = int.Parse(Console.ReadLine());
+    //int catFood = int.Parse(Console.ReadLine())*4;
+    //double fullPrice = (dogFood * 2.50)+catFood;
+    //Console.WriteLine($"{fullPrice} lv.");
+
+    //////Zadacha 09. Yard Greening                                                                                                                            Result: 100/100
+    //double kvMetri = double.Parse(Console.ReadLine())*7.61;
+    //double discount = kvMetri * 0.18;
+    //double finalPrice = kvMetri - discount;
+    //Console.WriteLine($"The final price is: {finalPrice} lv.\nThe discount is: {discount} lv.");
+
+}
+    }
+}
